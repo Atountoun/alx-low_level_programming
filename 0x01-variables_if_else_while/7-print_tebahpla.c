d632372d49b467202df4019c7253d9ff0f3eb1be
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:27:37 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,13 +6,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char c = 'z';
-	while(c >= 'a'){
+	
+	while(c >= 'a')
+	{
 		putchar(c);
 		c--;
 	}
+	
 	putchar('\n');
+	
 	return (0);
 }
-
