676f8be8bbd9ec3a5f285541fb5ce3f0b281573e
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:21:45 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,18 +6,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 
 	int i = '0';
 	int j = '0';
 	int k = '0';
-	for(; i<= '7'; i++){
-		for(j=i+1; j<='8'; j++){
-			for(k=j+1; k<='9'; k++){
+	for(; i<= '7'; i++)
+	{
+		for(j=i+1; j<='8'; j++)
+		{
+			for(k=j+1; k<='9'; k++)
+			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if(i != '7' || j!= '8' || k!='9'){
+				if(i != '7' || j!= '8' || k!='9')
+				{
 					putchar(',');
 					putchar(' ');
 				}
