3ca79dca62c678ee7aaa4dc68942142d80134ddc
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:24:56 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	
 	char c = 'a';
-	while(c <= 'z'){
-		if(c != 'e' && c != 'q'){
+	while(c <= 'z')
+	{
+		if(c != 'e' && c != 'q')
+		{
 			putchar(c);
 		}
 		c++;
 	}
+	
 	putchar('\n');
 	
 	return (0);
