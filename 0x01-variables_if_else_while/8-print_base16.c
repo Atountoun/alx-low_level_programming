b72bc4065766928c1b19f7642ca8818446f142ea
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:26:44 +0000

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,14 +5,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
 	int n = '0';
 	char l = 'a';
 
-	for(; n<= '9'; n++) putchar(n);
-	for(; l<= 'f'; l++) putchar(l);
+	for(; n<= '9'; n++)
+	{ 
+		putchar(n);
+	}
+	for(; l<= 'f'; l++)
+	{
+		putchar(l);
+	}
+	
 	putchar('\n');
 
 	return (0);
