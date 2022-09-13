301db25076ebcae51dfd263bba90714d31b91da9
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:24:11 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,21 +6,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	
 	char c = 'a';
 	/* Print first lowercase letters */
-	while(c <= 'z'){
+	while(c <= 'z')
+	{
 		putchar(c);
 		c++;
 	}
 	c = 'A';
 	/* Print uppercase letters */
-	while(c <= 'Z'){
+	while(c <= 'Z')
+	{
 		putchar(c);
 		c++;
 	}
+	
 	putchar('\n');
 
 	return (0);
