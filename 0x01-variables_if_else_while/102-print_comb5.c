92d99936e97a3485cb25a3eb8bc4598ae90798ff
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-13 10:22:48 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,22 +6,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 
 	int i = '0';
 	int j = '0';
 	int k = '0';
 	int l = '0';
-	for(i='0'; i<= '9'; i++){
-		for(j='0'; j<='8'; j++){
-			for(k='0'; k<='9'; k++){
-				for(l='0'; l<='9'; l++){
+	for(i='0'; i<= '9'; i++)
+	{
+		for(j='0'; j<='8'; j++)
+		{
+			for(k='0'; k<='9'; k++)
+			{
+				for(l='0'; l<='9'; l++)
+				{
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(k);
 					putchar(l);
-					if(i != '9' || j!= '8' || k!='9' || l!='9'){
+					if(i != '9' || j!= '8' || k!='9' || l!='9')
+					{
 						putchar(',');
 						putchar(' ');
 					}
