c9efccee45ed383cc69153f42e1ef6dae02e9f7c
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-21 01:46:08 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,26 +9,26 @@
  * @dest : a pointer to the string where to copy the content of @src
  * @src : a pointer to the string to be copied
  *
- * Return : the pointer to dest
+ * Return : char *
  */
 char *_strcpy(char *dest, char *src)
 {
-    int length;
-    int i=0;
+        int length;
+        int i = 0;
 
-    while (*src != '\0')
-    {
-            length++;
-            src++;
-    }
-    src = src - length;
+        while (*src != '\0')
+        {
+               length++;
+               src++;
+        }
+        src = src - length;
 
-    while (i<length)
-    {
-        *(dest + i) = *(src + i);
-        i++;
-    }
-    dest[i] = '\0';
+        while (i < length)
+        {
+                *(dest + i) = *(src + i);
+                i++;
+        }
+        dest[i] = '\0';
 
-    return dest;
+        return (dest);
 }
