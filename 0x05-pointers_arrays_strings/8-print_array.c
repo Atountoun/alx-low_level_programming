bf49923624ba3bac8ad557e9023f6a97e94dc47c
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-09-21 01:41:33 +0000

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,11 +18,11 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		printf("%d", *(a + i));
-		if (i != n-1)
+		if (i != n - 1)
 		{
 			printf(", ");
 		}
 		i++;
 	}
-	_putchar('\n');	
+	printf("\n");
 }
