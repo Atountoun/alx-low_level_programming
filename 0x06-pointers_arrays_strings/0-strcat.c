0a323521730a196f20c6b780408800af7373daef
Atountoun <tilltates8@gmail.com>
2022-09-23 07:26:23 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,26 +15,18 @@ char *_strcat(char *dest, char *src)
 	int src_length = 0;
 	int dest_length = 0;
 	int i = 0;
-	
-	while (*src != '\0')
-        {
-                src_length++;
-                src++;
-        }
-	while (*dest != '\0')
-	{
+
+	while (src[src_length] != '\0')
+		src_length++;
+
+	while (dest[dest_length] != '\0')
 		dest_length++;
-		dest++;
-	}
-	src -= src_length;
-	dest -= dest_length;
-	
+
 	while (i < src_length)
 	{
 		*(dest + i + dest_length) = *(src + i);
 		i++;
 	}
-	dest[i] = '\0';
 
 	return (dest);
 }
