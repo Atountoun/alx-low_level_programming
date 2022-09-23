4f7f9f71538aca4e2f04eedd030809df36bacef9
Atountoun <tilltates8@gmail.com>
2022-09-23 06:15:16 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,40 +15,27 @@ int _strcmp(char *s1, char *s2)
 	int length, count = 0, first_length = 0, second_length = 0;
 
 	while (s1[first_length} != '\0')
-	{
 		first_length++;
-	}
+
 	while (s2[second_length] != '\0')
-	{
 		second_length++;
-	}
+
 	if (first_length < second_length)
-	{
 		length = first_length;
-	}
 	else
-	{
 		length = second_length;
-	}
 	while (count < length)
 	{
 		if (s1[count] < s2[count])
-		{
 			return (-15);
-		}
-		else if (*s1 > *s2)
-		{
+		else if (s1[count] > s2[count])
 			return (15);
-		}
 		count++;
 	}
 	if (first_length < second_length)
-	{
 		return (-15);
-	}
 	else if (first_length > second_length)
-	{
 		return (15);
-	}
+
 	return (0);
 }
