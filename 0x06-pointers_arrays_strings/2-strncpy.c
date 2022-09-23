9a3c41c63414246c4d194ba196f768be801dd550
Atountoun <tilltates8@gmail.com>
2022-09-23 06:53:55 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,26 +5,26 @@
 /**
  * _strncpy - copies a string
  *
- * @dest : string which will be replaced by the content of @src
- * @src : string source
- * @n : number of bytes to be copied
+ * @dest: string which will be replaced by the content of @src
+ * @src: string source
+ * @: number of bytes to be copied
  *
- * Return : a pointer to the result
+ * Return: a pointer to the result
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int length = 0;
-	int i=0;
+	int i = 0;
 
-	while (*src != '\0')
+	while (src[length] != '\0')
+		length++;
+	while (i < length && i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
 	if (n > length)
-	{
 		dest[i] = '\0';
-	}
 
 	return (dest);
 }
