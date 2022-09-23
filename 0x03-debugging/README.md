7b7c56d4c12ba9923de5f0ac4b3edeea67c02a95
Atountoun <tilltates8@gmail.com>
2022-09-23 09:13:34 -0700

    Debugging task

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+# 0x03. C - Debugging
+## Tasks
+### 0. Multiple mains 
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void print_remaining_days(int m, int d, int y);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
