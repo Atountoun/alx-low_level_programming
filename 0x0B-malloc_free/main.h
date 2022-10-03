7b1ec6c4a4e4afb733089ad07430c237f1458c24
Atountoun <tilltates8@gmail.com>
2022-10-03 04:14:01 -0700

    creating files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+
+#endif /* MAIN_H_INCLUDED */
