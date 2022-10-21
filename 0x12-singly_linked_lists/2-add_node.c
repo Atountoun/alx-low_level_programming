13a55713c2fa245c6643d8115b547cb61861ccd0
Atountoun <tilltates8@gmail.com>
2022-10-20 17:57:42 -0700

    Fix errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,24 +3,6 @@
 #include "lists.h"
 #include <string.h>
 
-int len(char *str);
-list_t *add_node(list_t **head, const char *str);
-
-/**
- * len - counts the number of characters of a string
- * @str: points to a string
- *
- * Return: length of the string
- */
-int len(char *str)
-{
-	int length = 0;
-
-	while (str[length] != '\0')
-		length++;
-
-	return (length);
-}
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: points to the first element of the list
@@ -31,19 +13,23 @@ int len(char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup_str;
-	int length;
+	unsigned int length = 0;
 	list_t *node;
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
+
 	dup_str = strdup(str);
+
 	if (dup_str == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
-	length = len(str);
+	while (str[length] != '\0')
+		length++;
+
 	node->str = dup_str;
 	node->len = length;
 	node->next = *head;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,24 +3,6 @@
 #include "lists.h"
 #include <string.h>
 
-int len(char *str);
-list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * len - calculates the length of a string
- * @str: points to the string
- *
- * Return: the length of the string
- */
-int len(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 /**
  * add_node_end - adds a new node at the end of list_t list
  * @head: points to the first element of the list
@@ -31,19 +13,24 @@ int len(char *str)
 list_t *add_node_end(list_t **head, const char *str)
 {
 	char *dup;
-	int length;
+	unsigned int length = 0;
 	list_t node;
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
+
 	dup = strdup(str);
+
 	if (dup == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
-	length = len(str);
+
+	while (str[length] != '\0')
+		length++;
+
 	node->str = dup;
 	node->len = length;
 	node->next = NULL;
