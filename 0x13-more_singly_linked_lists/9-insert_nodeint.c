cd9470b4e9743d66a14137eac6c6e790a123870f
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-10-26 11:16:59 +0000

    fixing zero case error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current = *head;
 	new->n = n;
 
+	if (idx == 0)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
+
 	while (pos < idx)
 	{
 		if (current == NULL)
