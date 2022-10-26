6cbc4b080a17f9ad2df0c57a76f70842ec772564
Atountoun <111057734+Atountoun@users.noreply.github.com>
2022-10-26 11:19:18 +0000

    Fix index error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (pos < idx)
+	while (pos < idx - 1)
 	{
 		if (current == NULL)
 			return (NULL);
