e6cbd1646b23fbf97d8b447d7e35df8183ba48b3
Atountoun <tilltates8@gmail.com>
2022-11-16 05:34:05 -0800

    Append text to file function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+
+	if (op == -1 | wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
